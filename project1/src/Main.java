3716ecf598c3d22785a63b883f274be5c853eb1f
guillermokrh <guillermo.krh@gmail.com>
2018-03-16 21:57:26 -0500

    place jenkins&gitlab code inside repo for loop

--- project1/src/Main.java
+++ project1/src/Main.java
@@ -113,22 +113,17 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        /*
 
         GitHubClient client = new GitHubClient();
-        client.setOAuth2Token("80e841e804324f06e4755b7f05066f9dd0443e47");
-        System.out.println("Remaining requests: " + client.getRequestLimit());
 
-        // Download code from GitHub
+        //Initialize service to Download code from GitHub
         RepositoryService service = new RepositoryService();
         service.getClient();
 
         ContentsService contentsService = new ContentsService();
 
+        String query = "maven junit language:java";
 
-        String java_query = "language:java";
-        //Query searches for the simple-java-maven-app project inside the jenkins-docs org
-        String query = "simple-java-maven-app org:jenkins-docs";
         List<SearchRepository> searchRepositoryList = null;
 
         try {
@@ -138,173 +133,145 @@ public class Main {
             System.err.println("Caught IOException: " + e.getMessage());
         }
 
+        //Create a Jenkins Build for each repository
         for (SearchRepository searchRepo: searchRepositoryList) {
-            //If repository contains a Jenkinsfile, download the repository
-            System.out.println(searchRepo.getName() + " Watchers: " + searchRepo.getWatchers());
-            System.out.println("Homepage: " + searchRepo.getHomepage());
 
-            //Able to get repository URL so I can download repository using JGit
-            System.out.println("URL: " + searchRepo.getUrl());
-            System.out.println("To String: " + searchRepo.toString());
-            System.out.println("Owner: " + searchRepo.getOwner());
-            System.out.println("Name: " + searchRepo.getName());
-            System.out.println("ID: " + searchRepo.getId());
+            //TODO Update following variables based on repositories from Github search
+            String projectName = searchRepo.getName();
+            String username = searchRepo.getOwner();
 
-            //Create RepositoryID object based on current repo being searched
-            RepositoryId repositoryId = new RepositoryId(searchRepo.getOwner(), searchRepo.getName());
-            System.out.println("Repo ID: " + repositoryId.toString());
+            // TODO Please update the LOCAL_PATH variable for your setup
+            String repoURL = "https://github.com/" + username + "/" + projectName + ".git";
+            String LOCAL_PATH = "/home/virenmody/ClonedRepos/" + projectName;
+
+            // Clone the Github repository (RESOURCE: http://www.vogella.com/tutorials/JGit/article.html)
+            Git clonedRepo = null;
             try {
-                //Get the repository contents of that repo
-                List<RepositoryContents> repositoryContents = contentsService.getContents(repositoryId);
-
-                //Print the file names of the contents of the repo
-                for (RepositoryContents repo: repositoryContents){
-                    System.out.println("Get Name: " + repo.getName());
-                }
-
+                System.out.println("Cloning GitHub repo " + projectName + " to local directory: " + LOCAL_PATH);
+                clonedRepo = Git.cloneRepository()
+                        .setURI(repoURL)
+                        .setDirectory(new File(LOCAL_PATH))
+                        .call();
+            } catch (GitAPIException e) {
+                System.err.println("Caught GitAPIException: " + e.getMessage());
             }
-            catch (IOException e){
+
+            //TODO Please update the following for your setup
+            String gitlabHostUrl = "http://10.0.2.15";
+            String apiAccessToken = "RDtCnzMezmjpih4u6VDm";
+
+
+            // Retrieve Gitlab API Instance
+            GitLabApi gitLabApi = null;
+            try {
+                gitLabApi = new GitLabApi(GitLabApi.ApiVersion.V3, gitlabHostUrl, apiAccessToken);
+                System.out.println("Gitlab API Instance = " + gitLabApi);
+            } catch (Exception e) {
+                System.err.println("Caught Exception for Gitlab API Access: " + e.getMessage());
+                e.printStackTrace();
+            }
+
+            // Create a new empty project on Gitlab
+            Project newProject = null;
+            ProjectApi projectApi = null;
+            if (gitLabApi != null) {
+                projectApi = gitLabApi.getProjectApi();
+            }
+            // TODO Remove code that deletes all Gitlab projects
+            try {
+                List<Project> projectList = projectApi.getProjects();
+                for (Project p : projectList) {
+                    System.out.println("Deleting Project: " + p.getName());
+                    projectApi.deleteProject(p);
+                }
+            } catch (GitLabApiException e) {
+                System.err.println("Caught Exception for Gitlab Project API: " + e.getMessage());
+            }
+
+
+            //TODO First check if this project name already exists in Gitlab
+            String projectDescription = "Pulled from Github.com: " + username + " : " + projectName;
+            //String importUrl = "https://github.com/amuniz/maven-helloworld.git";
+            System.out.println("Creating Gitlab Project: " + projectName + " at " + gitlabHostUrl + "/root/"+projectName);
+            try {
+                newProject = projectApi.createProject(projectName, null, projectDescription, null, null, null, null, Visibility.PUBLIC, null, null);
+                //newProject = projectApi.createProject(projectName, null, projectDescription, null, null, null, null, Visibility.PUBLIC, null, importUrl);
+            } catch (GitLabApiException e) {
+                System.err.println("Caught GitlabApiException for Project Creation: " + e.getMessage());
+            }
+
+            // Create a Jenkins webhook for the project
+            //TODO Please update the localhost/ip:port number according to your Jenkins setup (localhost = 10.0.2.15)
+            String jenkinsHostUrl = "http://10.0.2.15:8081";
+            // TODO change jenkinsJob to projectName
+            String jenkinsJob = projectName;
+            //String jenkinsJob = "TestFreestyle";
+            String webhookUrl = jenkinsHostUrl + "/project/" + jenkinsJob;
+            ProjectHook webhook = null;
+            System.out.println("Creating Gitlab to Jenkins webhook: " + webhookUrl);
+            try {
+                webhook = projectApi.addHook(newProject, webhookUrl, true, false, false);
+            } catch (GitLabApiException e) {
+                System.err.println("Caught GitlabApiException for Webhook: " + e.getMessage());
+            }
+
+            // Create a Jenkins Job for the project
+            // TODO Please update the username and password according to your Jenkins setup
+            String jenkinsUsername = "admin";
+            String jenkinsPassword = "admin";
+            System.out.println("Creating a Jenkins job for this project: " + jenkinsHostUrl + "/job/" + jenkinsJob);
+            try {
+                URI jenkinsUri = new URI(jenkinsHostUrl + "/");
+                JenkinsServer jenkins = null;
+                jenkins = new JenkinsServer(jenkinsUri, jenkinsUsername, jenkinsPassword);
+                System.out.println("\t - Jenkins Running?: " + jenkins.isRunning()  );
+                String jobXml = customJenkinsJobXML(gitlabHostUrl, projectName);
+                jenkins.createJob(projectName, jobXml);
+                Map<String, Job> jobs = jenkins.getJobs();
+                System.out.println("\t - Jobs: " + jobs);
+
+            } catch (URISyntaxException e) {
+                System.err.println("Caught URISyntaxException for Jenkins Server Retrieval: " + e.getMessage());
+            } catch (IOException e) {
+                System.err.println("Caught IOException for Jenkins jobs retrieval: " + e.getMessage());
+                e.printStackTrace();
+            }
+
+
+            // Push repository to our local Gitlab server
+            System.out.println("Pushing project to Gitlab repository: " + projectName);
+            try {
+                //  Remove origin from repo (RESOURCE: https://stackoverflow.com/questions/12799573/add-remote-via-jgit)
+                System.out.println("\t - Removing remote Github repo as origin from repo");
+                StoredConfig config = clonedRepo.getRepository().getConfig();
+                config.unsetSection("remote", "origin");
+                config.save();
+
+                // Push the repo to Gitlab (Resource: https://stackoverflow.com/questions/13446842/how-do-i-do-git-push-with-jgit)
+                System.out.println("\t - Adding remote Gitlab repo as origin to repo");
+
+                RemoteAddCommand remoteAddCommand = clonedRepo.remoteAdd();
+                remoteAddCommand.setName("origin");
+                remoteAddCommand.setUri(new URIish(gitlabHostUrl + "/root/" + projectName + ".git"));
+                remoteAddCommand.call();
+
+                System.out.println("\t - Pushing repo with proper credentials to Gitlab Server");
+                // TODO Please update Gitlab username and password according to your Gitlab setup
+                String gitLabUsername = "root";
+                String gitLabPassword = "rootroot";
+                UsernamePasswordCredentialsProvider credProvider = new UsernamePasswordCredentialsProvider(gitLabUsername, gitLabPassword);
+                PushCommand pushCommand = clonedRepo.push();
+                pushCommand.setCredentialsProvider(credProvider);
+                pushCommand.call();
+
+            } catch (GitAPIException e) {
+                System.err.println("Caught GitAPIException: " + e.getMessage());
+            } catch (URISyntaxException e) {
+                System.err.println("Caught URISyntaxException: " + e.getMessage());
+            } catch (IOException e) {
                 System.err.println("Caught IOException: " + e.getMessage());
             }
         }
-        */
 
-        //TODO Update following variables based on repositories from Github search
-        String projectName = "example-groovy";
-        String username = "codecov";
-
-        // TODO Please update the LOCAL_PATH variable for your setup
-        String repoURL = "https://github.com/" + username + "/" + projectName + ".git";
-        //String repoURL = "https://github.com/guillermokrh/simple-java-maven-app";
-        String LOCAL_PATH = "/home/virenmody/ClonedRepos/" + projectName;
-        //String LOCAL_PATH = "/Users/guillermo/cs540/jgit_testing/";
-
-        // Clone the Github repository (RESOURCE: http://www.vogella.com/tutorials/JGit/article.html)
-        Git clonedRepo = null;
-        try {
-            System.out.println("Cloning GitHub repo " + projectName + " to local directory: " + LOCAL_PATH);
-            clonedRepo = Git.cloneRepository()
-                    .setURI(repoURL)
-                    .setDirectory(new File(LOCAL_PATH))
-                    .call();
-        } catch (GitAPIException e) {
-            System.err.println("Caught GitAPIException: " + e.getMessage());
-        }
-
-        //TODO Please update the following for your setup
-        String gitlabHostUrl = "http://10.0.2.15";
-        String apiAccessToken = "RDtCnzMezmjpih4u6VDm";
-
-
-        // Retrieve Gitlab API Instance
-        GitLabApi gitLabApi = null;
-        try {
-            gitLabApi = new GitLabApi(GitLabApi.ApiVersion.V3, gitlabHostUrl, apiAccessToken);
-            System.out.println("Gitlab API Instance = " + gitLabApi);
-        } catch (Exception e) {
-            System.err.println("Caught Exception for Gitlab API Access: " + e.getMessage());
-            e.printStackTrace();
-        }
-
-        // Create a new empty project on Gitlab
-        Project newProject = null;
-        ProjectApi projectApi = null;
-        if (gitLabApi != null) {
-            projectApi = gitLabApi.getProjectApi();
-        }
-        // TODO Remove code that deletes all Gitlab projects
-        try {
-            List<Project> projectList = projectApi.getProjects();
-            for (Project p : projectList) {
-                System.out.println("Deleting Project: " + p.getName());
-                projectApi.deleteProject(p);
-            }
-        } catch (GitLabApiException e) {
-            System.err.println("Caught Exception for Gitlab Project API: " + e.getMessage());
-        }
-
-
-        //TODO First check if this project name already exists in Gitlab
-        String projectDescription = "Pulled from Github.com: " + username + " : " + projectName;
-        //String importUrl = "https://github.com/amuniz/maven-helloworld.git";
-        System.out.println("Creating Gitlab Project: " + projectName + " at " + gitlabHostUrl + "/root/"+projectName);
-        try {
-            newProject = projectApi.createProject(projectName, null, projectDescription, null, null, null, null, Visibility.PUBLIC, null, null);
-            //newProject = projectApi.createProject(projectName, null, projectDescription, null, null, null, null, Visibility.PUBLIC, null, importUrl);
-        } catch (GitLabApiException e) {
-            System.err.println("Caught GitlabApiException for Project Creation: " + e.getMessage());
-        }
-
-        // Create a Jenkins webhook for the project
-        //TODO Please update the localhost/ip:port number according to your Jenkins setup (localhost = 10.0.2.15)
-        String jenkinsHostUrl = "http://10.0.2.15:8081";
-        // TODO change jenkinsJob to projectName
-        String jenkinsJob = projectName;
-        //String jenkinsJob = "TestFreestyle";
-        String webhookUrl = jenkinsHostUrl + "/project/" + jenkinsJob;
-        ProjectHook webhook = null;
-        System.out.println("Creating Gitlab to Jenkins webhook: " + webhookUrl);
-        try {
-            webhook = projectApi.addHook(newProject, webhookUrl, true, false, false);
-        } catch (GitLabApiException e) {
-            System.err.println("Caught GitlabApiException for Webhook: " + e.getMessage());
-        }
-
-        // Create a Jenkins Job for the project
-        // TODO Please update the username and password according to your Jenkins setup
-        String jenkinsUsername = "admin";
-        String jenkinsPassword = "admin";
-        System.out.println("Creating a Jenkins job for this project: " + jenkinsHostUrl + "/job/" + jenkinsJob);
-        try {
-            URI jenkinsUri = new URI(jenkinsHostUrl + "/");
-            JenkinsServer jenkins = null;
-            jenkins = new JenkinsServer(jenkinsUri, jenkinsUsername, jenkinsPassword);
-            System.out.println("\t - Jenkins Running?: " + jenkins.isRunning()  );
-            String jobXml = customJenkinsJobXML(gitlabHostUrl, projectName);
-            jenkins.createJob(projectName, jobXml);
-            Map<String, Job> jobs = jenkins.getJobs();
-            System.out.println("\t - Jobs: " + jobs);
-
-        } catch (URISyntaxException e) {
-            System.err.println("Caught URISyntaxException for Jenkins Server Retrieval: " + e.getMessage());
-        } catch (IOException e) {
-            System.err.println("Caught IOException for Jenkins jobs retrieval: " + e.getMessage());
-            e.printStackTrace();
-        }
-
-
-        // Push repository to our local Gitlab server
-        System.out.println("Pushing project to Gitlab repository: " + projectName);
-        try {
-            //  Remove origin from repo (RESOURCE: https://stackoverflow.com/questions/12799573/add-remote-via-jgit)
-            System.out.println("\t - Removing remote Github repo as origin from repo");
-            StoredConfig config = clonedRepo.getRepository().getConfig();
-            config.unsetSection("remote", "origin");
-            config.save();
-
-            // Push the repo to Gitlab (Resource: https://stackoverflow.com/questions/13446842/how-do-i-do-git-push-with-jgit)
-            System.out.println("\t - Adding remote Gitlab repo as origin to repo");
-
-            RemoteAddCommand remoteAddCommand = clonedRepo.remoteAdd();
-            remoteAddCommand.setName("origin");
-            remoteAddCommand.setUri(new URIish(gitlabHostUrl + "/root/" + projectName + ".git"));
-            remoteAddCommand.call();
-
-            System.out.println("\t - Pushing repo with proper credentials to Gitlab Server");
-            // TODO Please update Gitlab username and password according to your Gitlab setup
-            String gitLabUsername = "root";
-            String gitLabPassword = "rootroot";
-            UsernamePasswordCredentialsProvider credProvider = new UsernamePasswordCredentialsProvider(gitLabUsername, gitLabPassword);
-            PushCommand pushCommand = clonedRepo.push();
-            pushCommand.setCredentialsProvider(credProvider);
-            pushCommand.call();
-
-        } catch (GitAPIException e) {
-            System.err.println("Caught GitAPIException: " + e.getMessage());
-        } catch (URISyntaxException e) {
-            System.err.println("Caught URISyntaxException: " + e.getMessage());
-        } catch (IOException e) {
-            System.err.println("Caught IOException: " + e.getMessage());
-        }
     }
 }
